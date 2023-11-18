2720bd9a478d13a5e02a7bc874e71dce4c49dc3c
Leaversa <mtkwong@cpp.edu>
2023-11-17 20:04:02 -0800

    refactor navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react';
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu" // Update with the correct path
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
-  // state to track the currently active button
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
-  const handleButtonClick = (buttonIndex: number) => {
-    setActiveIndex((prevActiveIndex) => prevActiveIndex === buttonIndex ? null : buttonIndex);
-  };
-
   return (
-    <NavigationMenu className="text-white p-4">
-      <NavigationMenuList>
-        {Array.from({ length: 6 }, (_, idx) => (
-          <NavigationMenuItem key={idx}>
-            <NavigationMenuTrigger
-              onClick={() => handleButtonClick(idx)}
-              className={`${activeIndex === idx ? 'your-active-class' : ''}`}
-            >
-              <span className="text-lg font-semibold">serverName</span>
-            </NavigationMenuTrigger>
-          </NavigationMenuItem>
+    <ScrollArea className="w-max h-screen mx-2 overflow-none hide-scrollbar overflow-y-auto">
+      <div className="flex flex-col mx-1 py-2">
+        <Button variant="avatar" className="p-0 h-24 w-24 border-4 border-yellow-500 my-1">
+          <Avatar className="h-full w-full">
+            <AvatarImage className="object-cover h-fullpointer-events-none" src="" />
+            <AvatarFallback className="w-full h-full bg-inherit font-semibold text-2xl text-muted">First</AvatarFallback>
+          </Avatar>
+        </Button>
+        {Array.from({ length: 15 }, (_, idx) => (
+          <Button variant="avatar" className="p-0 h-24 w-24 hover:border-4 hover:border-yellow-500 my-1" key={idx}>
+            <Avatar className="h-full w-full">
+              <AvatarImage className="object-cover h-full pointer-events-none" src="https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg" />
+              <AvatarFallback className="w-full h-full bg-inherit font-semibold text-2xl text-muted">Server</AvatarFallback>
+            </Avatar>
+          </Button>
         ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+      </div>
+      <ScrollBar hidden="false"/>
+    </ScrollArea>
   );
 }

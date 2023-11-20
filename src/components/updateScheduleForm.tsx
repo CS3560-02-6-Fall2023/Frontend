657020196442ba09f7e913b72c99708c99e032dd
alimomennasab <alimomennasab3@gmail.com>
2023-11-19 22:54:02 -0800

    Added updating schedule feature template

---
+++ src/components/updateScheduleForm.tsx
@@ -0,0 +1,40 @@
+// Component for updating the user's schedule.
+
+import { Button } from "./ui/button";
+import { Dialog, DialogContent, DialogTrigger, DialogClose } from "./ui/dialog";
+import { Label } from "@radix-ui/react-label";
+
+const UpdateScheduleForm = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <div className="mb-4">
+          <Label htmlFor="update-schedule" className="block text-lg font-bold mb-2">
+            Update Your Schedule
+          </Label>
+          <div className="mb-4">
+            <Label htmlFor="email">You currently have no courses.</Label>
+          </div>
+          <div className="mb-4">
+            <Button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+              Add Course
+            </Button>
+          </div>
+          <div className="flex justify-between space-x-2">
+            <DialogClose asChild>
+              <Button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-1/2">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-1/2">
+              Save and Exit
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UpdateScheduleForm;
--- src/components/userDetails.tsx
+++ src/components/userDetails.tsx
@@ -1,32 +1,82 @@
 import { Button } from "./ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Dialog } from "./ui/dialog";
+import { DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "./ui/input";
+import UpdateScheduleForm from "./updateScheduleForm";
 
-export default function userDetails() {
+export function DialogDemo({ children }: { children: React.ReactNode }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <div className="py-3">
+          <div className="mb-4">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" />
+          </div>
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password" />
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex space-x-2 ">
+              <Checkbox id="remember-me" />
+              <Label
+                htmlFor="remember-me"
+                className="text-sm text-muted-foreground"
+              >
+                Remember me?
+              </Label>
+            </div>
+            <Button
+              variant="link"
+              className="text-muted-foreground text-sm font-medium"
+            >
+              Forgot password?
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default function UserDetails() {
+
   return (
     <div className="border-t border-gray-700 pt-4">
       <p className="mb-2">User</p>
       <Popover>
         <PopoverTrigger asChild>
-          <Button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">User Settings</Button>
+          <Button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            User Settings
+          </Button>
         </PopoverTrigger>
         <PopoverContent className="w-80 rounded-xl">
           <div className="grid gap-4">
             <div className="space-y-2">
-              <h4 className="font-semibold text-xl text-center leading-none">User Settings</h4>
+              <h4 className="font-semibold text-xl text-center leading-none">
+                User Settings
+              </h4>
               <div className="flex items-center space-x-5 my-2">
                 <Avatar>
                   <AvatarImage src="" />
                   <AvatarFallback>U</AvatarFallback>
                 </Avatar>
                 <div className="">
-                    <p>USERNAME</p>
-                    <p>EMAIL@cpp.edu</p>
+                  <p>USERNAME</p>
+                  <p>EMAIL@cpp.edu</p>
                 </div>
               </div>
             </div>
             <div className="grid gap-3">
-              <Button className="text-lg">Update Schedule</Button>
+              <UpdateScheduleForm>
+                <Button className="text-lg">Update Schedule</Button>
+              </UpdateScheduleForm>
               <Button className="text-lg ">Change Profile Image</Button>
               <Button className="text-lg ">Log Out</Button>
             </div>

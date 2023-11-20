206b4bd4009fb870a5d8077e142a22fab628fbd4
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2023-11-20 14:55:24 -0800

    feat: added auth context component

---
+++ src/components/authenticationProvider.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable react-refresh/only-export-components */
+import React from "react";
+
+interface UserType {
+    userID: string;
+    username: string;
+}
+
+interface UserContextType extends UserType {
+    setUser: React.Dispatch<React.SetStateAction<UserType>>;
+}
+
+interface AuthenticatedContextType {
+    authenticated: boolean;
+    setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const UserContext = React.createContext<UserContextType>({
+    userID: "",
+    username: "",
+    setUser: () => {},
+});
+
+export const AuthenticatedContext =
+    React.createContext<AuthenticatedContextType>({
+        authenticated: false,
+        setAuthenticated: () => {},
+    });
+
+export function AuthenticationProvider({
+    children,
+}: {
+    children: JSX.Element;
+}) {
+    const [user, setUser] = React.useState<UserType>({
+        userID: "",
+        username: "",
+    });
+    const [authenticated, setAuthenticated] = React.useState(false);
+
+    return (
+        <AuthenticatedContext.Provider
+            value={{ authenticated, setAuthenticated }}
+        >
+            <UserContext.Provider value={{ ...user, setUser }}>
+                {children}
+            </UserContext.Provider>
+        </AuthenticatedContext.Provider>
+    );
+}
